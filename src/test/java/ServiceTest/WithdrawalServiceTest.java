c148143c113d77e36cd13999fcb43c760b5aa3e8
elief <elie.faniar@stud.sti.se>
2025-05-09 13:08:26 +0200

    commit User story 3. parprogrammering ( elie driver , Jawhar navigator ) withdraw class ,tester och ändringar i Account

--- src/test/java/ServiceTest/WithdrawalServiceTest.java
+++ src/test/java/ServiceTest/WithdrawalServiceTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-
+//parprogrammeirng
 
 import static org.junit.Assert.assertEquals;
 
